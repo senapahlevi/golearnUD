2632189ba265f86caf32f443f4d7d4efb483b19c
ezypolis.senapahlevi <sena.pahlevi@ezypolis.com>
2022-05-27 10:18:28 +0700

    learn map

--- main.go
+++ main.go
@@ -4,8 +4,10 @@ import "fmt"
 
 func main() {
 	m := map[string]int{"key": 5}
+	// fmt.Println(m)
+	m["key"] = 10 //ini add/nambah stack
+	// fmt.Println(m)
+	delete(m, "key") //ini delete dan delete dari awal array
+	m["newsss"] = 2  //ini juga add key
 	fmt.Println(m)
-	m["key"] = 10
-	fmt.Println(m)
-
 }
