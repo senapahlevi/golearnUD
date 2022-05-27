6f36bca68fcd1684cceada8c8c1f5f17145711ad
ezypolis.senapahlevi <sena.pahlevi@ezypolis.com>
2022-05-27 15:34:56 +0700

    gorm Model

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -16,49 +17,19 @@ func main() {
 		panic("Cannot connect to DB")
 	}
 	DB.AutoMigrate(&User{})
-	//quering these comment immediately after succes input 3 data into postgresql
-	// users := []User{
-	// 	{
-	// 		FirstName: "Sena",
-	// 		LastName:  "Pahlevi",
-	// 		Email:     "senapahlevi@gmail.com",
-	// 	},
-	// 	{
-	// 		FirstName: "Xenna",
-	// 		LastName:  "Smith",
-	// 		Email:     "xennasmith@gmail.com",
-	// 	},
-	// 	{
-	// 		FirstName: "William",
-	// 		LastName:  "Blake",
-	// 		Email:     "williamblake@gmail.com",
-	// 	},
-	// }
-	// //these assign above 3 datas
-	// for _, user := range users { //these using underscore(_) if didnt want to use variable because on docs
-	// 	//for range always using variables to avoid error coz golang is type programming language (Strict not like .js)
-	// 	DB.Create(&user)
-	// }
 
-	//query look like filter (first,last,where(similiarity like filters))
-	user := User{} //these var user will assign and to printout like .push() create new array zero to got output
-	//first() showing first datas
-	// DB.First(&user) //always passing &user coz to asssign into var user
-	// fmt.Println(user)
-
-	//last showing last data
-	// DB.Last(&user) //always passing &user coz to asssign into var user
-	// fmt.Println(user)
-
-	//query filter using where
-	DB.Where("last_name", "Smith").First(&user) //why last_name not Lastname like these struct? coz it point out the column postgress last_name
+	// gorm.Model
+	user := User{
+		Model: gorm.Model{
+			CreatedAt: time.Now(),
+		},
+	}
 	fmt.Println(user)
-
 }
 
 type User struct {
-	Id        int
-	FirstName string
-	LastName  string
-	Email     string
+	gorm.Model //so these gorm model will automatically add Id, delete/update At
+	FirstName  string
+	LastName   string
+	Email      string
 }
