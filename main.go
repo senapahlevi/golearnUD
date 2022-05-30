c88d99790f2f234b97ab75d15d5a8ebe3cca4a6d
ezypolis.senapahlevi <sena.pahlevi@ezypolis.com>
2022-05-30 10:16:58 +0700

    many to many relation book

--- main.go
+++ main.go
@@ -17,21 +17,39 @@ func main() {
 	}
 	// DB.AutoMigrate(&User{})
 	//these used to drop and migrate like migrate:refresh laravel
-	DB.Migrator().DropTable(&User{}, &Address{})
-	DB.Migrator().CreateTable(&User{}, &Address{})
+	// DB.Migrator().DropTable(&User{}, &Address{})
+	// DB.Migrator().CreateTable(&User{}, &Address{})
+	DB.Migrator().DropTable(&User{}, &Book{})
+	DB.Migrator().CreateTable(&User{}, &Book{})
+	//kalo ga bisa migrator make auto migrate kadang suka aneh sih antara itu biar bisa migrate gitu
+	// DB.AutoMigrate(&User{}, &Book{})
 	// gorm.Model
 	//test buat nullstring(sql.nullable) bukan nullable
+	// user := User{
+	// 	FirstName: "sena",
+	// 	LastName:  "pahlevvvvv",
+	// 	Email:     "senapahlevi2@gmail.com",
+	// 	Address: Address{
+	// 		Name: "Main str.",
+	// 	},
+	// }
 	user := User{
 		FirstName: "sena",
 		LastName:  "pahlevvvvv",
 		Email:     "senapahlevi2@gmail.com",
-		Address: Address{
-			Name: "Main str.",
+		Books: []Book{
+			{
+				Title: "Bookeeee", //masuk ke user_books
+			},
+			{
+				Title: "Buku harry potter 2", //masuk ke user_books
+			},
 		},
 	}
 	DB.Create(&user)
 	u := User{}
-	DB.Preload("Address").First(&u)
+	// DB.Preload("Address").First(&u)
+	DB.Preload("Books").First(&u)
 	fmt.Println(u)
 }
 
@@ -44,17 +62,19 @@ func main() {
 // }
 
 //using sql.NullString when condition null/not null will gives warning or like handling errors so the data will not save with empty data
+//many to many
 type User struct {
 	gorm.Model //so these gorm model will automatically add Id, delete/update At
 	// ID        uint
-	FirstName string  `gorm:"type:VARCHAR(30); null;"`
-	LastName  string  `gorm:"size:100; default:'Smith weberjenkinson'"`
-	Email     string  `gorm:"unique; not null;"`
-	Address   Address `gorm:"foreignKey:UserId"`
+	FirstName string `gorm:"type:VARCHAR(30); null;"`
+	LastName  string `gorm:"size:100; default:'Smith weberjenkinson'"`
+	Email     string `gorm:"unique; not null;"`
+	// Address   Address `gorm:"foreignKey:UserId"`
+	Books []Book `gorm:"many2many:user_books"` //many to many
 }
-type Address struct {
+type Book struct {
 	gorm.Model //so these gorm model will automatically add Id, delete/update At
 	// ID     uint
-	UserId uint
-	Name   string
+	ID    uint
+	Title string
 }
