3f74bda5c498625fbd0edcf6d86b779c7fc0a6de
ezypolis.senapahlevi <sena.pahlevi@ezypolis.com>
2022-06-01 22:06:37 +0700

    go admin json output

--- models/user.go
+++ models/user.go
@@ -1,10 +1,10 @@
 package models
 
 type User struct {
-	Id        uint
-	FirstName string
-	LastName  string
-	Email     string `gorm:"unique"`
+	Id        uint   `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email" gorm:"unique"`
 	// Password  string
-	Password []byte
+	Password []byte `json:"-"` //- is not showing output after user regis/login in jsonusing these coz password on Register func authController avoid error if using string coz using hash
 }
