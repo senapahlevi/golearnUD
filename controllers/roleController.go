8e650c2c1c23100a462cef0f83e308701db5e7c5
ezypolis.senapahlevi <sena.pahlevi@ezypolis.com>
2022-06-13 15:46:25 +0700

    add many to many and fixing some issue on previous permission

--- controllers/roleController.go
+++ controllers/roleController.go
@@ -17,12 +17,26 @@ func AllRoles(c *fiber.Ctx) error {
 }
 
 func CreateRole(c *fiber.Ctx) error {
-	var role models.Role
+	var roleDto fiber.Map
 
-	if err := c.BodyParser(&role); err != nil {
+	// var role models.Role
+
+	// if err := c.BodyParser(&role); err != nil {
+	if err := c.BodyParser(&roleDto); err != nil {
 		return err
 	}
-
+	list := roleDto["permissions"].([]interface{})
+	permissions := make([]models.Permission, len(list))
+	for i, permissionId := range list {
+		id, _ := strconv.Atoi(permissionId.(string))
+		permissions[i] = models.Permission{
+			Id: uint(id),
+		}
+	}
+	role := models.Role{
+		Name:        roleDto["name"].(string),
+		Permissions: permissions,
+	}
 	database.DB.Create(&role)
 	return c.JSON(role)
 }
@@ -32,19 +46,39 @@ func GetRole(c *fiber.Ctx) error {
 	role := models.Role{
 		Id: uint(id),
 	}
-	database.DB.Find(&role) //find user ex user/4 , user/3
+	// database.DB.Find(&role) //find user ex user/4 , user/3
+	database.DB.Preload("Permissions").Find(&role)
+
 	return c.JSON(role)
 }
 
 func UpdateRole(c *fiber.Ctx) error {
 	id, _ := strconv.Atoi(c.Params("id")) //we get id from params convert back again strting to unsigned avoid error when assign user := models.User{}
-	role := models.Role{
-		Id: uint(id),
-	}
-
-	if err := c.BodyParser(&role); err != nil {
+	// role := models.Role{
+	// 	Id: uint(id),
+	// }
+	var roleDto fiber.Map
+	// if err := c.BodyParser(&role); err != nil {
+	if err := c.BodyParser(&roleDto); err != nil {
 		return err
 	} //to regist input new for update
+	list := roleDto["permissions"].([]interface{})
+	permissions := make([]models.Permission, len(list))
+	for i, permissionId := range list {
+		id, _ := permissionId.(float64)
+		permissions[i] = models.Permission{
+			Id: uint(id),
+		}
+	}
+	var result interface{}
+	//deletes the old one to avoid redundant/still saving in db after update assign value new on roles_permission
+	database.DB.Table("role_permissions").Where("role_id", id).Delete(&result)
+	//after delete the old one now create new
+	role := models.Role{
+		Id:          uint(id),
+		Name:        roleDto["name"].(string),
+		Permissions: permissions,
+	}
 	database.DB.Model(&role).Updates(role)
 	return c.JSON(role)
 }
@@ -55,5 +89,6 @@ func DeleteRole(c *fiber.Ctx) error {
 	}
 
 	database.DB.Delete(&role)
-	return c.JSON(role)
+	// return c.JSON(role)
+	return nil
 }
--- models/permission.go
+++ models/permission.go
@@ -4,3 +4,5 @@ type Permission struct {
 	Id   uint   `json:"id"`
 	Name string `json:"name"`
 }
+
+//permission in db postgre assign manual ex col id name: 1, view_products
